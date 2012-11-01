d78a2b7ac73d7ade2f6d113566f8b2ce669f21e7
Janko Marohnić <janko.marohnic@gmail.com>
2012-11-01 22:34:00 +0100

    Remove actual credentials from version controls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require 'active_support/core_ext/hash/keys'
 RSPEC_DIR = File.expand_path(File.dirname(__FILE__))
 Dir[File.join(RSPEC_DIR, "support/**/*.rb")].each { |f| require f }
 
+CONFIG = YAML.load(File.read("#{RSPEC_DIR}/config.yml")).symbolize_keys
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
 end
@@ -14,8 +16,8 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :fakeweb
   config.default_cassette_options = {serialize_with: :syck}
-  config.filter_sensitive_data('API_KEY')      { ENV['DROPBOX_APP_KEY'] }
-  config.filter_sensitive_data('ACCESS_TOKEN') { ENV['DROPBOX_ACCESS_TOKEN'] }
+  config.filter_sensitive_data('API_KEY')      { CONFIG[:app_key] }
+  config.filter_sensitive_data('ACCESS_TOKEN') { CONFIG[:app_secret] }
   config.configure_rspec_metadata!
   config.allow_http_connections_when_no_cassette = true
 end

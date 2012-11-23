3372e4f9ba4c5a400c3a7de460ab27052b46fbc5
Alex Grant <alexgrant@apprentice.io>
2012-11-23 13:37:38 -0500

    Support app_folder access level

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,31 @@
 require 'vcr'
 require 'yaml'
 require 'erb'
+require 'rest_client'
+require 'active_support/core_ext/hash/except'
 require 'active_support/core_ext/hash/keys'
 
 RSPEC_DIR = File.expand_path(File.dirname(__FILE__))
 Dir[File.join(RSPEC_DIR, "support/**/*.rb")].each { |f| require f }
 
-CREDENTIALS_FILE = "#{RSPEC_DIR}/dropbox.yml"
+CREDENTIAL_FILES = {
+  dropbox: "#{RSPEC_DIR}/dropbox.yml",
+  app_folder: "#{RSPEC_DIR}/app_folder.yml"
+}
 
-if File.exists?(CREDENTIALS_FILE)
-  CREDENTIALS = YAML.load(ERB.new(File.read(CREDENTIALS_FILE)).result).symbolize_keys
+if CREDENTIAL_FILES.all? { |_, file| File.exists?(file) }
+  CREDENTIALS = Hash[CREDENTIAL_FILES.map do |key, file|
+    [key, YAML.load(ERB.new(File.read(file)).result).symbolize_keys]
+  end]
+
+  if not CREDENTIALS.all? { |mode, keys| keys[:access_level].to_sym == mode }
+    puts error_message_wrong_access_levels
+    exit
+  end
 else
-  puts <<-EOS
-
-### ERROR ###
-Credential file not found at spec/dropbox.yml.
-Copy spec/dropbox.yml.example and fill in your credentials.
-
-  EOS
+  puts error_message_credentials_not_found
   exit
 end
-
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
@@ -29,8 +34,10 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :fakeweb
-  CREDENTIALS.keys.each do |key|
-    config.filter_sensitive_data("<#{key.to_s.upcase}>") { CREDENTIALS[key] }
+  CREDENTIALS.each do |mode, keys|
+    keys.except(:access_level).each do |key, value|
+      config.filter_sensitive_data("<#{mode.to_s.upcase}_#{key.to_s.upcase}>") { value }
+    end
   end
   config.default_cassette_options = {
     serialize_with: :syck,
